39a4c4cddea563aadd4bf50c0203ab20264882f9
Miguelanggelo7 <miguelanggelo21@gmail.com>
2025-10-03 00:11:19 +0200

    BuyOneGetOneFree rule done

--- lib/rules/buy_one_get_one_free_rule.rb
+++ lib/rules/buy_one_get_one_free_rule.rb
@@ -1,9 +1,30 @@
+require 'bigdecimal'
+require 'product'
+
 class BuyOneGetOneFreeRule
   def initialize(product_code)
     @product_code = product_code
   end
 
   def apply(items)
-    items
+    result = []
+    counter = 0
+
+    items.each do |item|
+      if item.code == @product_code
+        counter += 1
+        # Cada segunda unidad es gratis
+        if counter.even?
+          # Creamos una copia del producto pero con precio 0
+          result << Product.new(item.code, item.name, BigDecimal("0"))
+        else
+          result << item
+        end
+      else
+        result << item
+      end
+    end
+
+    result
   end
 end
